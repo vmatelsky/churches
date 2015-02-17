66704c545fab76d27c3f5cae9dff0fed4f6021cb
Matelsky, Vladislav <VMatelsky@qulix.com>
2015-02-17 11:31:53 +0300

    fix obtaining map instance

--- Churches/com.churches.by/src/main/java/com/churches/by/ChurchesMap.java
+++ Churches/com.churches.by/src/main/java/com/churches/by/ChurchesMap.java
@@ -2,7 +2,7 @@ package com.churches.by;
 
 import android.location.Address;
 import android.location.Location;
-import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 
@@ -17,6 +17,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.vla3089.functional.Receiver;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsMenu;
 
@@ -26,20 +27,19 @@ import java.util.List;
 @OptionsMenu(R.menu.menu_churches_map)
 public class ChurchesMap extends ActionBarActivity {
 
-    private int userIcon, foodIcon, drinkIcon, shopIcon, otherIcon;
+    private static final int userIcon = R.drawable.yellow_point;
+    private static final int foodIcon = R.drawable.red_point;
+    private static final int drinkIcon = R.drawable.blue_point;
+    private static final int shopIcon = R.drawable.green_point;
+    private static final int otherIcon = R.drawable.purple_point;
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-    private Marker userMarker;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        userIcon = R.drawable.yellow_point;
-        foodIcon = R.drawable.red_point;
-        drinkIcon = R.drawable.blue_point;
-        shopIcon = R.drawable.green_point;
-        otherIcon = R.drawable.purple_point;
+    public ChurchesMap() {
+    }
 
+    @AfterViews
+    protected void setupMap() {
         if(mMap==null){
             FragmentManager fm = getSupportFragmentManager();
             SupportMapFragment mapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
@@ -51,7 +51,7 @@ public class ChurchesMap extends ActionBarActivity {
 
         if (mMap != null) {
             mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            updatePlaces();
+            mMap.setMyLocationEnabled(true);
             displayChurches();
         }
     }
@@ -78,24 +78,4 @@ public class ChurchesMap extends ActionBarActivity {
         super.onResume();
     }
 
-    private void updatePlaces(){
-        Location lastLoc = CAppliation.locationManager().lastKnownLocation();
-
-        double lat = lastLoc.getLatitude();
-        double lng = lastLoc.getLongitude();
-
-        LatLng lastLatLng = new LatLng(lat, lng);
-
-        if(userMarker!=null) {
-            userMarker.remove();
-        }
-
-        userMarker = mMap.addMarker(new MarkerOptions()
-                .position(lastLatLng)
-                .title("You are here")
-                .icon(BitmapDescriptorFactory.fromResource(userIcon))
-                .snippet("Your last recorded location"));
-
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(lastLatLng), 3000, null);
-    }
 }
