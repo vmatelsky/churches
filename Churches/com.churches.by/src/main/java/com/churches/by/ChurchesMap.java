62399a2ff9bebcc74153fc34f828c0707023e2ba
Matelsky, Vladislav <VMatelsky@qulix.com>
2015-03-06 13:39:59 +0300

    add toolbar

--- Churches/com.churches.by/src/main/java/com/churches/by/ChurchesMap.java
+++ Churches/com.churches.by/src/main/java/com/churches/by/ChurchesMap.java
@@ -1,50 +1,63 @@
 package com.churches.by;
 
-import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.churches.by.data.DataProvider;
 import com.churches.by.data.model.Church;
+import com.churches.by.ui.BaseActivity;
 import com.churches.by.ui.Map;
-import com.vla3089.functional.Receiver;
 
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.FragmentById;
-import org.androidannotations.annotations.OptionsMenu;
+public class ChurchesMap extends BaseActivity implements Map.OnMapInteractionListener, Toolbar.OnMenuItemClickListener {
 
-import java.util.ArrayList;
-import java.util.List;
-
-@EActivity(R.layout.activity_churches_map)
-@OptionsMenu(R.menu.menu_churches_map)
-public class ChurchesMap extends ActionBarActivity implements Map.OnMapInteractionListener {
-
-    @FragmentById(R.id.church_map)
-    Map churchesMap;
+    private DrawerLayout drawer;
 
     public ChurchesMap() {
     }
 
-    /*@OptionsItem(R.id.action_settings)
-    protected void startIntentService() {
-        Intent intent = new Intent(this, FetchAddressIntentService.class);
-        intent.putExtra(FetchAddressIntentService.REQUEST_ADDRESS, "г. Минск, пл. Свободы 9");
-        startService(intent);
-    }*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    @AfterViews
-    protected void displayChurches() {
-        DataProvider.instance().requestChurches(new Receiver<List<Church>>() {
-            @Override
-            public void receive(List<Church> value) {
-                churchesMap.updateDisplayableChurches(new ArrayList<>(value));
-            }
-        });
+        setActionBarIcon(R.drawable.ic_drawer);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        drawer.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_churches_map, menu);
+        return true;
     }
 
     @Override
     public void onChurchInfoClicked(Church church) {
         Toast.makeText(this, church.name() + " clicked", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(Gravity.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        Toast.makeText(this, menuItem.getTitle() + " clicked", Toast.LENGTH_LONG).show();
+        return false;
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_churches_map;
     }
 }
