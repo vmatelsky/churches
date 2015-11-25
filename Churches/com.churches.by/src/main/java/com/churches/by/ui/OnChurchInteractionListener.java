26709ce356be5144382905623db27409cfc6d5a2
Matelsky, Vladislav <VMatelsky@qulix.com>
2015-11-25 22:16:30 +0300

    remove unused dialog

--- Churches/com.churches.by/src/main/java/com/churches/by/ui/OnChurchInteractionListener.java
+++
@@ -1,9 +0,0 @@
-package com.churches.by.ui;
-
-import com.churches.by.data.model.Church;
-
-public interface OnChurchInteractionListener {
-    void onChurchClicked(Church church);
-
-    void onSettingsClicked();
-}
